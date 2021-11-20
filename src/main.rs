398ab615ff1b940daba80df1dacc33c12a14bcce
Adrian <adrian.boston@gmail.com>
2021-11-20 01:21:00 -0800

    All new files dumped into output directory and each image saved in new directory filename w leagacy.ext and 320w-1440w files

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 
 The srcset utility generates multiple (eight) scaled versions of an image at particular breakpoints -- 320,480,640,768,960,1024,1280,1440 pixels wide -- that match common Mobile and widescreen viewports using Imagemagick's convert utility and outputs the needed <img> tag.
 
-    str='<img src="'$outprefix$srcsuffix.$type'" srcset="'$outprefix-320w.$type' 320w, '$outprefix-480w.$type' 480w, '$outprefix-640w.$type' 640w, '$outprefix-768w.$type' 768w, '$outprefix-960w.$type' 960w, '$outprefix-1024w.$type' 1024w, '$outprefix-1440w.$type' 1440w" sizes="'$sizes'" alt="An image named '$filename'"/>'
-
-
 */
 
 //extern crate argparse;
@@ -19,31 +16,22 @@ use std::ffi::OsStr;
 use argparse::{ArgumentParser, StoreTrue, Store};
 use image::{GenericImageView};
 
-// ./srset.sh [-hmz] [-q quality] [—t type] [-l legacysize] [-s sizes] [-o out path] [-f filename] filename
 
-// ./srset.sh [-hmz] [—n findname] [-q quality] [—t type] [-l legacysize] [-s sizes] [-o out path] [-f file hierarchy] file hierarchy
-
-//
 struct Options<'a> {
-    inpath: &'a Path,
     outpath: &'a Path,
-    
     extension: String,
-    
+    sizes: String,
     is_recurse: bool,
-    
     is_test: bool,
 }
 
 
-
-
 fn main() {
 
-    let mut inpath_str = "filename".to_string();
-    let mut outpath_str = ".".to_string();
+    let mut inpath_str = "".to_string();
+    let mut outpath_str = "".to_string();
     let mut extension = "".to_string();
-
+    let mut sizes = "(min-width: 768px) 50vw, 100vw".to_string();
     let mut is_recurse = false;
     let mut is_test = false;
 
@@ -54,7 +42,7 @@ fn main() {
 
         args.refer(&mut inpath_str)
                 .required()
-                .add_option(&["-p", "--path"], Store,
+                .add_option(&["-f", "--file"], Store,
                 "Path (Filename or directory) of image");
  
         args.refer(&mut outpath_str)
@@ -69,38 +57,37 @@ fn main() {
                 .add_option(&["-t", "--type"], Store,
                 "Output filetype (jpg, png, etc)");
 
+        args.refer(&mut sizes)
+                .add_option(&["-s", "--sizes"], Store,
+                "The src viewport sizes tag as string");
+
         args.refer(&mut is_test)
                 .add_option(&["-z", "--test"], StoreTrue,
                 "Test run. Images are found but not created");
 
         args.parse_args_or_exit();
     }
+    
+    // Output must end in /
+    if !outpath_str.ends_with("/") {  outpath_str.push_str("/"); }
 
     let outpath = Path::new(&outpath_str);
     if outpath.is_file() {
         println!("Selected outpath is a file");
         std::process::exit(1);
     }
-    // Make sure outpath has suffix '/'
-    if outpath.ends_with("/") {
-        println!("Remove trailing /");
-    }
-
-    //println!("Outpath parent {}, filename {}, ", outpath.parent().unwrap().to_str().unwrap(), outpath.file_stem().and_then(OsStr::to_str).unwrap(),  );
-        
 
     let inpath = Path::new(&inpath_str);
 
-    let options = Options {inpath, outpath, extension, is_recurse, is_test};
+    let options = Options {outpath, extension, sizes, is_recurse, is_test};
 
-    
-    if inpath.is_dir() {
-        let _noop = loop_path(&inpath, &outpath, &options);
-    }
-    else
+    let result =
+    match inpath.is_dir()
     {
-        process_image(&inpath, &outpath, &options);
-    }
+        true => loop_path(&inpath, &options),
+        _ => process_image(&inpath, &options),
+    };
+    println!("{:?}", result);
 
 }
 
@@ -110,7 +97,7 @@ fn main() {
 /// \param dir          The path of the directory
 /// \param outpath      The path of the output directory
 /// \param is_recurse   Whether to recurse directory
-fn loop_path(dir: &Path, outpath: &Path, options: &Options)  -> io::Result<()>
+fn loop_path(dir: &Path, options: &Options)  -> io::Result<()>
 {
     
     if dir.is_dir() {
@@ -118,12 +105,15 @@ fn loop_path(dir: &Path, outpath: &Path, options: &Options)  -> io::Result<()>
             let entry = entry?;
             let path = entry.path();
             if options.is_recurse && path.is_dir() {
-                loop_path(&path, &outpath, &options)?;
+                loop_path(&path, &options)?;
             } else {
+                // Directories dont have extensions?! so will simply continue
                 match path.extension().and_then(OsStr::to_str)
                 {
                     None => (),
-                    Some("jpg") | Some("JPG") | Some("png") | Some("PNG") => process_image(&path, &outpath, &options),
+                    Some("jpg") | Some("JPG") | Some("png") | Some("PNG")
+                        | Some("tiff") | Some("TIFF")
+                            => process_image(&path, &options)?,
                     _ => (),
                 }
 
@@ -131,6 +121,7 @@ fn loop_path(dir: &Path, outpath: &Path, options: &Options)  -> io::Result<()>
             }
         }
     }
+    
     Ok(())
 
 }
@@ -138,9 +129,9 @@ fn loop_path(dir: &Path, outpath: &Path, options: &Options)  -> io::Result<()>
 /// \fn             process_image(path: &Path)
 /// \brief          Process one image
 ///
-/// \param dir          The path of the directory
+/// \param dir          The path of the current file
 /// \param is_recurse   Whether to recurse directory
-fn process_image(path: &Path, outpath: &Path, options: &Options)
+fn process_image(path: &Path, options: &Options) -> io::Result<()>
 {
     let file_path = path.to_str().unwrap();
 
@@ -159,10 +150,22 @@ fn process_image(path: &Path, outpath: &Path, options: &Options)
         let aspect =  w as f32 / h as f32;
     
         println!("\nProcessing image {}: {:?} {:.2} {:?}", file_path, wh, aspect, img.color());
+    
+        // Outpath must exist. Then create directory based on image name (not incuding extension)
+        let j = options.outpath.join(path.file_stem().and_then(OsStr::to_str).unwrap());
+        let p = j.as_path();
+        if !p.exists() {
+            std::fs::create_dir(p)?;
+        }
+    
+        // Saving the legacy image
+        let legacy_name = format!("{}{}/legacy.{}",options.outpath.to_str().unwrap(), path.file_stem().and_then(OsStr::to_str).unwrap(), get_file_ext(&path, &options) );
+        img.save(legacy_name).unwrap();
 
         // 320,480,640,768,960,1024,1280,1440 pixels wide
+        // Iterate through the sizes and create a scaled image for each
         for n in sizes {
-            scale_and_save(&path, &outpath, &img, n as u32, (n as f32 / aspect) as u32, &options);
+            scale_and_save(&path, &options.outpath, &img, n as u32, (n as f32 / aspect) as u32, &options);
         }
     }
     else
@@ -170,6 +173,8 @@ fn process_image(path: &Path, outpath: &Path, options: &Options)
         println!("\nProcessing image {}", file_path);
     }
     
+    
+    // End of iteration
     let file_name = path.file_stem().and_then(OsStr::to_str).unwrap();
     let file_ext: &str;
     if options.extension == "" {
@@ -179,13 +184,14 @@ fn process_image(path: &Path, outpath: &Path, options: &Options)
     }
     
     // Produce the output path of new files
-    println!("Images found in {0}{1}/320w.{2}, and 480w.{2}, 640w.{2}, 768w.{2}, 960w.{2}, 1024w.{2}, 1280w.{2}, 1440w.{2}", outpath.to_str().unwrap(), file_name, file_ext);
+    println!("Images placed in {0}{1}/320w.{2}, and /480w.{2}, /640w.{2}, /768w.{2}, /960w.{2}, /1024w.{2}, /1280w.{2}, 1440w.{2}", options.outpath.to_str().unwrap(), file_name, file_ext);
 
     // Now output the srcset tag
-    let srcset_tag = format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w, {0}/1440w.{1} 1440w\" sizes=\"\" alt=\"A file named {0}\">", file_name, file_ext);
+    let srcset_tag = format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w, {0}/1440w.{1} 1440w\" sizes=\"{2}\" alt=\"A file named {0}\">", file_name, file_ext, options.sizes);
 
     println!("Output srcset tag\n{}", srcset_tag);
-
+    
+    Ok(())
 }
 
 /// \fn             scale_and_save(path: &Path, img: &image::DynamicImage, nwidth: u32, nheight: u32)
@@ -198,47 +204,39 @@ fn process_image(path: &Path, outpath: &Path, options: &Options)
 
 fn scale_and_save(path: &Path, outpath: &Path, img: &image::DynamicImage, nwidth: u32, nheight: u32, options: &Options)
 {
-    let file_parent = path.parent().unwrap().to_str().unwrap();
-//    println!("  Parent {:?}", file_parent);
     
-//    println!("  Outpath {:?}", outpath.parent().unwrap().to_str().unwrap());
-
-//    if outpath == path.parent().unwrap() {
-//        println!("  Root is the same");
-//    }
-    
-//    let file_name = path.file_name();
-     let file_name = path.file_stem().and_then(OsStr::to_str).unwrap();
-//     println!(" Filename {:?}", file_name);
+    let file_name = path.file_stem().and_then(OsStr::to_str).unwrap(); //?
 
     // The new path from names, sizes and file ext
-//    let new_name = format!("{}/{}-{}x{}.{}",file_parent, file_name, nwidth, nheight, file_ext);
     let file_ext: &str;
     if options.extension == "" {
-        file_ext = path.extension().and_then(OsStr::to_str).unwrap();
+        file_ext = path.extension().and_then(OsStr::to_str).unwrap(); //?
     } else {
         file_ext = options.extension.as_str();
     }
 
     let new_name = format!("{}{}/{}w.{}",outpath.to_str().unwrap(), file_name, nwidth, file_ext);
     
-    println!("New image {}", new_name);
-
     let scaled = img.resize_to_fill(nwidth, nheight, image::imageops::FilterType::Lanczos3);
-    // Write the contents of this image to the Writer in format found in extension.
-    scaled.save(new_name).unwrap();
 
+    // Write the contents of this image in format found in file_ext.
+    scaled.save(&new_name).unwrap();
 
-
+    println!("Created {}", new_name);
 }
 
 
 
-#[allow(dead_code)]
-fn extension_from_path(path: &Path) -> Option<&str>
+// The new path from names, sizes and file ext
+fn get_file_ext<'life>(path: &'life Path, options: &'life Options) -> &'life str
 {
-    path.extension().and_then(OsStr::to_str)
+    match options.extension.as_str()
+    {
+        "" => path.extension().and_then(OsStr::to_str).unwrap(),
+        _ => options.extension.as_str(),
+    }
 }
+
 
 #[allow(dead_code)]
 fn loop_dir(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()>
