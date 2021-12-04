03c5bc0249f9bc6d124f82a090aab076b73a4312
Adrian <adrian.boston@gmail.com>
2021-12-03 21:20:17 -0800

    split functions, added metrics

--- src/walk.rs
+++ src/walk.rs
@@ -3,90 +3,88 @@
 
 use std::path::Path;
 use std::ffi::OsStr;
+#[allow(unused_imports)]
 use std::fs::DirEntry;
 
-use crate::opts::Opts;
+use anyhow::Result;
+
+use crate::opts::{Opts, Metrics};
 use crate::img::process_image;
 
 
+/// Walk or traverse a provided directory. Calls itself recursively if a directory is found within
+/// the provided path, and the options specify.
+pub fn walk_path(dir: &Path,  opts: &Opts, m: &mut Metrics) -> Result<()>
+{
+    if dir.is_dir() {
+         // An error here (permission denied) will bail the walk. Dont bail the walk. Instead continue back to the parent
+        let rd = match std::fs::read_dir(dir) {
+            Ok(t) => t,
+            Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?}", e);} return Ok(())},
+        };
+        
+        for entry in rd {
 
-/// Walk a directory tree, hunting for jpg, png and tiff extensions
-pub fn walk_path(dir: &Path, opts: &Opts)  -> anyhow::Result<()>
+            m.traversed = m.traversed + 1;
+
+            let entry = match entry {
+                Ok(entry) => entry,
+                Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?}", e)}; continue;},
+            };
+            let path = entry.path();
+            if opts.is_recurse && path.is_dir() {
+                walk_path(&path, &opts, m)?;
+            } else {
+                digest_path(&path, &opts, m)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+/// Digest or consume a path. Check extension for image type (jpg, png, tiff). In addition,
+/// Skips any filename matching `"320w|480w|640w|768w|960w|1024w|1280w|1440w|legacy`
+/// If matching the above concerns, then process the iamge.
+/// Moves on without error if there is no match.
+pub fn digest_path(path: &Path, opts: &Opts, m: &mut Metrics) -> Result<()>
 {
     lazy_static::lazy_static! {
         static ref RE: regex::Regex = regex::Regex::new("320w|480w|640w|768w|960w|1024w|1280w|1440w|legacy").unwrap();
     }
 
-    if dir.is_dir() {
+    // Directories dont have extensions?! so will simply continue
+    match path.extension().and_then(OsStr::to_str)
+    {
+        // No extension. Move on
+        None => (),
+        
+        Some("jpg") | Some("JPG") | Some("png") | Some("PNG")
+            | Some("tiff") | Some("TIFF") | Some("tif") | Some("TIF")
+                => {
+                        if path.metadata().unwrap().len() > opts.min_size
+                        {
+                            let nm = path.file_stem().and_then(OsStr::to_str).unwrap();
 
-         // An error here (permission denied) will bail the walk. Dont bail the walk. Instead continue to the parent
-        let rd = match std::fs::read_dir(dir) {
-            Ok(t) => t,
-            Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?}", e);} return Ok(())},
-        };
-
-        for entry in rd {
-            //let dir = entry;
-            let entry = match entry {
-                Ok(entry) => entry,
-                Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?}", e)}; continue;},
-            };
-
-            let path = entry.path();
-
-            if opts.is_recurse && path.is_dir() {
-                walk_path(&path, &opts)?;
-            } else {
-                // Directories dont have extensions?! so will simply continue
-                match path.extension().and_then(OsStr::to_str)
-                {
-                    None => (),
-                    Some("jpg") | Some("JPG") | Some("png") | Some("PNG")
-                        | Some("tiff") | Some("TIFF") | Some("tif") | Some("TIF")
-                            => {
-                                    if path.metadata()?.len() > opts.min_size
-                                    {
-                                        let nm = path.file_stem().and_then(OsStr::to_str).unwrap();
-
-                                        if !RE.is_match(nm)
-                                        {
-                                            match process_image(&path, &opts) {
-                                                Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?} {:?}", path, e)}},
-                                                _ => (),
-                                        }
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if !opts.is_quiet{eprintln!("WARNING: Skipping {:?}", path)};
-                                    }
-                                }
-                                ,
-                    _ => (),
-                }
-
-
-            }
-        }
+                            // Make sure were not converting a previously converted image. Matching the filename
+                            if !RE.is_match(nm)
+                            {
+                                match process_image(&path, &opts, m) {
+                                    Err(e) => { if !opts.is_quiet{eprintln!("WARNING: Processing error {:?} {:?}", path, e)}},
+                                    _ => (),
+                            }
+                            }
+                        }
+                        else
+                        {
+                            m.skipped = m.skipped + 1;
+                            if !opts.is_quiet{eprintln!("WARNING: Skipping {:?}", path)};
+                        }
+                    }
+                    ,
+        // Anything else. Move on
+        _ => (),
     }
+    
 
-    Ok(())
-}
-
-#[allow(dead_code)]
-fn loop_dir(dir: &Path, cb: &dyn Fn(&DirEntry)) -> std::io::Result<()>
-{
-    if dir.is_dir() {
-        for entry in std::fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                loop_dir(&path, cb)?;
-            } else {
-                //println!("File {:?}", entry);
-                cb(&entry);
-            }
-        }
-    }
     Ok(())
 }
