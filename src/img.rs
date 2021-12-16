8f46fe08e7feb0311acf3432195b826f487168e2
Adrian <adrian.boston@gmail.com>
2021-12-16 15:52:19 -0800

    Cleaned up deadcode and improved image description output

--- src/img.rs
+++ src/img.rs
@@ -13,12 +13,11 @@ use crate::opts::{Opts, Metrics};
 use crate::utils::{use_fileext,mk_dir, path_from_array};
 
 
-
 /// Process the image provided in the path.
 /// Iterate through the sizes and create a scaled image for each
 pub fn process_image(path: &Path, opts: &Opts, m: &mut Metrics) -> Result<()>
 {
-    let all_sizes = [320, 480, 640, 768, 960, 1024, 1280, 1440];
+//    let _all_sizes = [480, 640, 768, 960, 1024, 1366, 1600, 1920];
     
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
@@ -35,10 +34,11 @@ pub fn process_image(path: &Path, opts: &Opts, m: &mut Metrics) -> Result<()>
     let aspect =  w as f32 / h as f32;
     
     // Pick maximum array slice based on width of provided image
-    let sizes = strip_sizes(w, all_sizes);
+    let sizes = strip_sizes(w);
     let max = *sizes.last().unwrap();
- 
-    println!("path len: {:?}, dimensions: {:?}, color:{:?}", wh,path.metadata().unwrap().len(), img.color());
+    let sz = path.metadata().unwrap().len();
+    
+    println!("Image: Width={}: Height={}; Size={}; Color={:?}", w, h, human_bytes::human_bytes(sz as f64), img.color());
 
     let ext = use_fileext(&path, &opts.extension);
     let file_name = path.file_stem().and_then(OsStr::to_str).unwrap();
@@ -172,24 +172,7 @@ pub fn scale_and_save(path: &Path, outpath: &Path,
     Ok(())
 }
 
-//    let sizes = [320, 480, 640, 768, 960, 1024, 1280, 1440];
-//    let sizes = [320, 480, 640, 768, 960, 1024, 1280, 1440];
-/*
-fn strip_sizes(max: u32) -> &'static [u32]
-{
-    match max {
-        d if d < 320 => &[320],
-        d if d < 480 => &[320,480],
-        d if d < 640 => &[320,480,640],
-        d if d < 768 => &[320,480,640,768],
-        d if d < 960 => &[320,480,640,768,960],
-        d if d < 1024 => &[320,480,640,768,960,1024],
-        d if d < 1280 => &[320,480,640,768,960,1024,1280],
-        d if d < 1440 => &[320,480,640,768,960,1024,1280,1440],
-        _ => &[320,480,640,768,960,1024,1280,1440],
-    }
-}
-
+/// Provide an array that is suitable for large and small images based on the width
 fn strip_sizes(max: u32) -> &'static [u32]
 {
     match max {
@@ -200,65 +183,25 @@ fn strip_sizes(max: u32) -> &'static [u32]
         d if d < 1024 => &[480,640,768,960,1024],
         d if d < 1366 => &[480,640,768,960,1024,1366],
         d if d < 1600 => &[480,640,768,960,1024,1366,1600],
-        _ => &[480,640,768,960,1024,1366,1600,1900],
-    }
-}
-*/
-
-fn strip_sizes(max: u32, sizes: &[&u32]) -> &'static [u32]
-{
-    println!("{:?}", sizes);
-    
-    match max {
-        d if d < 480 => &[480],
-        d if d < 640 => &[480,640],
-        d if d < 768 => &[480,640,768],
-        d if d < 960 => &[480,640,768,960],
-        d if d < 1024 => &[480,640,768,960,1024],
-        d if d < 1366 => &[480,640,768,960,1024,1366],
-        d if d < 1600 => &[480,640,768,960,1024,1366,1600],
-        _ => &[480,640,768,960,1024,1366,1600,1900],
+        _ => &[480,640,768,960,1024,1366,1600,1920],
     }
 }
 
-
+/// Provide an <img srcset=""> tag with the image names, smaller images get smaller sets of images
 fn create_tag<'a>(max: u32, f: &'a str, ext: &'a str, n: &'a str) -> String
 {
-//        let tag = format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w, {0}/1440w.{1} 1440w\" sizes=\"{2}\" alt=\"A file named {3}\">", sp.to_str().unwrap(), ext, opts.sizes, file_name);
-
-//(min-width: 768px) 50vw, 100vw
-
-    let s =
+    let tag =
     match max {
-        d if d < 320 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1}\" sizes=\"(max-width:320px) 100vw, min-width:321px) 25vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 480 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1}\" sizes=\"(max-width:480px) 100vw, min-width:481px) 25vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 640 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w\" sizes=\"(max-width:640px) 100vw, min-width:641px) 33vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 768 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w\" sizes=\"(max-width:320px) 50vw, (max-width:768px) 100vw, min-width:769px) 50vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 960 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 1024 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 1280 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
-        d if d < 1440 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w, {0}/1440w.{1} 1440w\" sizes=\"(min-width: 768px) 50vw, 100vw\" alt=\"A file named {2}\">",f, ext, n),
-        _ => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/320w.{1} 320w, {0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1280w.{1} 1280w, {0}/1440w.{1} 1440w\" sizes=\"(max-width: 768px) 33vw, (min-width: 768px) 50vw, 100vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 480 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1}\" sizes=\"(max-width:480px) 100vw, min-width:481px) 25vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 640 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w\" sizes=\"(max-width:640px) 100vw, min-width:641px) 33vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 768 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w\" sizes=\"(max-width:320px) 50vw, (max-width:768px) 100vw, min-width:769px) 50vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 960 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 1024 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 1366 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1366w.{1} 1366w\" sizes=\"(max-width:320px) 50vw, (max-width:960px) 75vw, (min-width:961px) 95vw\" alt=\"A file named {2}\">",f, ext, n),
+        d if d < 1660 => format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1366w.{1} 1366w, {0}/1600w.{1} 1600w\" sizes=\"(max-width:320px) 25vw, (min-width: 960px) 75vw, 100vw\" alt=\"A file named {2}\">",f, ext, n),
+        _ =>             format!("<img src=\"{0}/legacy.{1}\" srcset=\"{0}/480w.{1} 480w, {0}/640w.{1} 640w, {0}/768w.{1} 768w, {0}/960w.{1} 960w, {0}/1024w.{1} 1024w, {0}/1366w.{1} 1366w, {0}/1600w.{1} 1600w, {0}/1920w.{1} 1920w\" sizes=\"(min-width: 1024px) 50vw, 100vw\" alt=\"A file named {2}\">",f, ext, n),
     };
 
-    println!("{}",s);
-    s
+    println!("{}",tag);
+    tag
 }
-
-
-/*
-fn srcset_sizes(array: &[&u32])
-{
-    
-    match max {
-        d if d < 320 => &[320],
-        d if d < 480 => &[320,480],
-        d if d < 640 => &[320,480,640],
-        d if d < 768 => &[320,480,640,768],
-        d if d < 960 => &[320,480,640,768,960],
-        d if d < 1024 => &[320,480,640,768,960,1024,1280],
-        d if d < 1440 => &[320,480,640,768,960,1024,1280,1440],
-        _ => &[320,480,640,768,960,1024,1280,1440],
-    }
-}
-*/
